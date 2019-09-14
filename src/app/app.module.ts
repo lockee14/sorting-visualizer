f51242b9cb54c6c700f67a7e8fbeb991bc6ff047
mehdi <lockee14@msn.com>
2019-09-15 01:54:56 +0900

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/top-bar/top-bar.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+import { BehaviorSubject, Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+import { AlgoService } from '../algo.service';
+
+@Component({
+  selector: 'app-top-bar',
+  templateUrl: './top-bar.component.html',
+  styleUrls: ['./top-bar.component.scss']
+})
+export class TopBarComponent implements OnInit {
+  arrayLength = new BehaviorSubject<number>(4);
+  arrayLength$ = this.arrayLength.asObservable().pipe(tap((x) => console.log('arrayLength: ', this.arrayLength)));
+
+  selectedAlgo: string;
+  sorting$: Observable<boolean>;
+  constructor(
+    private algoService: AlgoService
+  ) { }
+
+  ngOnInit() {
+    this.sorting$ = this.algoService.sorting$;
+  }
+
+  changeArrayLength(e) {
+    this.algoService.generateArray(e.target.value);
+  }
+
+  generateArray() {
+    this.algoService.generateArray();
+  }
+
+  chooseAlgo(name) {
+    this.selectedAlgo = name;
+  }
+
+  sort() {
+    if (this.selectedAlgo) {
+      this.algoService[this.selectedAlgo]();
+    }
+  }
+
+}
